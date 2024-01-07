7315e1027669c2ff053777b980c8ab31ecd67537
avelando <avelarrodrigues89@gmail.com>
2024-01-06 23:46:41 -0300

    .

--- ordenacaoPython/ordenacao/selection_sort.py
+++
@@ -1,9 +0,0 @@
-def selection_sort(array):
-    for index in range(0, len(array)):
-        min_index = index
-
-        for right in range(index + 1, len(array)):
-            if array[right] < array[min_index]:
-                min_index = right
-
-        array[index], array[min_index] = array[min_index], array[index]
